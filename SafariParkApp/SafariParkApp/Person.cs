134d26817214648553deeab2d8e8b3fddb41215b
arthur tshaka <arthurltshaka@gmail.com>
2021-08-03 13:36:11 +0100

    first commit

---
+++ SafariParkApp/SafariParkApp/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SafariParkApp
+{
+	public class Person
+	{
+		private string _firstName;
+		private string _lastName;
+
+		public int Age { get; set; }
+		public Person(string firstName, string lastName, int age)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+			Age = age;
+		}
+		public string GetFullName()
+		{
+			return $"{ _firstName} {_lastName}";
+		}
+			
+		
+	}
+}
---
+++ SafariParkApp/SafariParkApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SafariParkApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person arthur = new Person("Arthur", "Tshaka", 23);
+			Console.WriteLine(arthur.GetFullName());
+		}
+	}
+}
